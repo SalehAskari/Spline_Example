c6e69dd9ab585c4c53cd32c3d6339961d274be1d
SALEH <133962662+SalehAskari@users.noreply.github.com>
2024-05-27 11:37:26 +0330

    Update Spline.py

--- Spline.py
+++ Spline.py
@@ -1,8 +1,6 @@
 import os.path as osp
-
 import torch
 import torch.nn.functional as F
-
 import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import SplineConv
@@ -12,21 +10,24 @@ path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
 dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
 data = dataset[0]
 
+# Add edge_attr if it's not present
+if not hasattr(data, 'edge_attr') or data.edge_attr is None:
+    # Initialize edge_attr with random values for the sake of example
+    # Typically, edge_attr should represent some meaningful properties of edges
+    data.edge_attr = torch.rand((data.edge_index.size(1), 2))
 
-class Spline(torch.nn.Module):
+class Net(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        # SplineConv requires additional parameters like dim and kernel_size
         self.conv1 = SplineConv(dataset.num_features, 16, dim=2, kernel_size=5)
         self.conv2 = SplineConv(16, dataset.num_classes, dim=2, kernel_size=5)
 
     def forward(self):
-        x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        x = F.relu(self.conv1(x, edge_index, edge_attr))
+        x = F.dropout(x,p=0.6, training=self.training)
+        x = self.conv2(x, edge_index, edge_attr)
         return F.log_softmax(x, dim=1)
-
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -35,16 +36,14 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
 else:
     device = torch.device('cpu')
 
-model, data = Spline().to(device), data.to(device)
+model, data = Net().to(device), data.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-
 
 def train():
     model.train()
     optimizer.zero_grad()
     F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
     optimizer.step()
-
 
 @torch.no_grad()
 def test():
@@ -56,9 +55,8 @@ def test():
         accs.append(acc)
     return accs
 
-
 best_val_acc = test_acc = 0
-for epoch in range(1, 201):
+for epoch in range(1, 20):
     train()
     train_acc, val_acc, tmp_test_acc = test()
     if val_acc > best_val_acc:
